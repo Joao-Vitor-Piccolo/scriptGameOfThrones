758ef807002d128fcde2a6379280aac547cc3ce7
Piccolo's <joaovitorpiccolo015@gmail.com>
2024-09-18 20:21:14 -0300

    Update scriptGameOfThrones.py

--- scriptGameOfThrones.py
+++ scriptGameOfThrones.py
@@ -6,15 +6,17 @@ class Character:
         self.name = name
         self.hair = hair
         self.region = region
+        self.strength = 3
+        self.instincts = None
+        self.speed = 2
 
 # // Targeryans -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 class Targeryan(Character):
     dragons = ['Drogon', 'Rhaegal', 'Viserion', 'Balerion', 'Vermithor', 'Caraxes',
                'Meraxes', 'Syrax', 'Meleys', 'Sunfyre', 'Silverwing', 'Dreamfyre',
                'Tessarion', 'Vhagar']
-
-    def __init__(self, name, hair, region, dragon=None):
-        super().__init__(name.title(), hair.title(), region.title())
+    def __init__(self, character, dragon=None):
+        super().__init__(character.name, character.hair, character.region)
         self.house = 'Targeryan'
         self.last_n = self.house
         self.dragon = dragon if dragon else choice(Targeryan.dragons)
@@ -25,6 +27,26 @@ class Targeryan(Character):
 
     def quotes(self):
         pass
+
+
+# // Starks   -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class Starks(Character):
+    wolves = ['Grey Wind', 'Lady', 'Nymeria', 'Summer', 'Shaggydog', 'Ghost']
+
+    def __init__(self, character, wolf=None):
+        super().__init__(character.name, character.hair, character.region)
+        self.wolf = wolf if wolf else choice(Starks.wolves)
+        self.warg = True
+        self.house = 'Stark'
+        self.last_n = self.house
+
+    def warg_into_wolf(self):
+        if self.wolf:
+            self.name = self.wolf
+            self.strength = 9
+            self.speed = 9
+            self.instincts = True
 
 # // Bastards -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -56,23 +78,23 @@ class Bastard(Character):
 
 
 # \\ Tests -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-name1 = 'Dany'
-hair1 = 'White'
-region1 = 'South'
-
-character1 = Character(name1, hair1, region1)
-if character1.hair.title() == 'White':
-    character1 = Targeryan(character1.name, character1.hair, character1.region)
-print(type(character1))
-
-for x in character1.__dict__:
-    print(f'{x.title()}: {character1.__dict__[x]}')
-
-print('=*=' * 20)
-
-bastard1 = Bastard('Ramsay', 'black', 'north')
-for x in bastard1.__dict__:
-    print(f'{x.title()}: {bastard1.__dict__[x]}')
-
-print(bastard1.say_quote())
+if __name__ == "__main__":
+    name1 = 'Dany'
+    hair1 = 'White'
+    region1 = 'South'
+    
+    character1 = Character(name1, hair1, region1)
+    if character1.hair.title() == 'White':
+        character1 = Targeryan(character1)
+    print(type(character1))
+    
+    for x in character1.__dict__:
+        print(f'{x.title()}: {character1.__dict__[x]}')
+    
+    print('=*=' * 20)
+    
+    bastard1 = Bastard('Ramsay', 'black', 'north')
+    for x in bastard1.__dict__:
+        print(f'{x.title()}: {bastard1.__dict__[x]}')
+    
+    print(bastard1.say_quote())
